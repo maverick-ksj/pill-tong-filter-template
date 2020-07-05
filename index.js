12dd664e457d608dd5b8bbc832a56432c5e28b9a
shj <kan02134@gmail.com>
2020-07-05 13:08:37 +0900

    Create template code

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * filter template code for pill-tong waf <https://github.com/maverick-ksj/pill-tong>
+ * 
+ * @author shj <https://github.com/Gumball12>
+ */
+
+module.exports = require('./lib/pill-tong-filter');
